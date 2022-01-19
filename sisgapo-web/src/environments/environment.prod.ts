ce0157300adbbc17b8509a4f309d96bebadfbfd2
Jmurga16 <58633633+Jmurga16@users.noreply.github.com>
2022-01-19 11:15:52 -0500

    Update environment.prod.ts

--- sisgapo-web/src/environments/environment.prod.ts
+++ sisgapo-web/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL_INV:"http://sisgapoback.azurewebsites.net/",
+  API_URL_INV:"https://sisgapo-api-2022.azurewebsites.net/",
+  //API_URL_INV:"http://sisgapoback.azurewebsites.net/",
 };
